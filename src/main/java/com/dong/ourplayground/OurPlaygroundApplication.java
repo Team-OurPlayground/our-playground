f7998556505ba81710259e657a9fae88e5bd7bad
dong young <dong5854@gmail.com>
2022-11-26 01:58:17 +0900

    init project

---
+++ src/main/java/com/dong/ourplayground/OurPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.dong.ourplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dong/ourplayground/OurPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dong.ourplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
