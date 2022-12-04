338dce6d2b6b2f876ae03470e21ac91512a17d59
dong young <dong5854@gmail.com>
2022-12-04 22:57:04 +0900

    feat: add redis chatRoom repository

--- src/main/java/com/dong/ourplayground/domain/chat/ChatRoomRepository.java
+++ src/main/java/com/dong/ourplayground/domain/chat/ChatRoomRepository.java
@@ -1,7 +1,9 @@
 package com.dong.ourplayground.domain.chat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("ChatRoomRepositoryRDB")
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     ChatRoom findChatRoomById(Long id);
 }
