04b8d0564b79113c3c794bee0407ba05b8c0eb63
dong young <dong5854@gmail.com>
2022-12-04 23:01:04 +0900

    feat: change ChatRoom Repository to redis template repository

--- src/main/java/com/dong/ourplayground/web/ChatRoomController.java
+++ src/main/java/com/dong/ourplayground/web/ChatRoomController.java
@@ -1,7 +1,8 @@
 package com.dong.ourplayground.web;
 
-import com.dong.ourplayground.domain.chat.ChatRoom;
-import com.dong.ourplayground.domain.chat.ChatRoomRepository;
+
+import com.dong.ourplayground.domain.chat.redistemplate.ChatRoom;
+import com.dong.ourplayground.domain.chat.redistemplate.ChatRoomRepository;
 import com.dong.ourplayground.web.dto.CreateRoomRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -14,6 +15,7 @@ import java.util.List;
 @RequestMapping("/chat")
 public class ChatRoomController {
 
+
     private final ChatRoomRepository chatRoomRepository;
 
     @GetMapping("/rooms")
@@ -25,6 +27,12 @@ public class ChatRoomController {
     @PostMapping("/room")
     @ResponseBody
     public ChatRoom createRoom(@RequestBody CreateRoomRequestDTO createRoomRequestDTO) {
-        return chatRoomRepository.save(createRoomRequestDTO.toEntity());
+        return chatRoomRepository.createChatRoom(createRoomRequestDTO.getName());
+    }
+
+    @GetMapping("/room/{roomID}")
+    @ResponseBody
+    public ChatRoom roomInfo(@PathVariable String roomID) {
+        return chatRoomRepository.findRoomById(roomID);
     }
 }
