3749f6afda25234cff24fa7359c25535c6089e4f
dong young <dong5854@gmail.com>
2022-12-16 01:56:55 +0900

    feat: security 임시 해제

--- src/main/java/com/dong/ourplayground/OurPlaygroundApplication.java
+++ src/main/java/com/dong/ourplayground/OurPlaygroundApplication.java
@@ -2,10 +2,11 @@ package com.dong.ourplayground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // TODO: 인증 개발 완료되면 활성화
 public class OurPlaygroundApplication {
 
 	public static void main(String[] args) {
